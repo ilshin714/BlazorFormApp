5668923c495c1fc3d1918b878e115817e4ef6d31
Ilshin.Ji <Ilshin.Ji@peergroup.com>
2022-06-16 13:28:55 -0400

    Add project files.

---
+++ BlazorFormApp/Models/ExampleItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFormApp.Models
+{
+    public class ExampleItem
+    {
+        [Required(ErrorMessage = "Name is requied")]
+        [StringLength(20, ErrorMessage = "Please type less than 20 characters.")]
+        public string Name { get; set; }
+    }
+}
